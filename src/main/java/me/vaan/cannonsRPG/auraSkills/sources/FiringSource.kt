0cafbd387c7a71fff2cff01aba8ac3c677d59835
Intybyte <vaan.testwork@gmail.com>
2024-08-31 23:05:16 +0200

    Complete the classes

--- src/main/java/me/vaan/cannonsRPG/auraSkills/sources/FiringSource.kt
+++ src/main/java/me/vaan/cannonsRPG/auraSkills/sources/FiringSource.kt
@@ -1,3 +1,18 @@
 package me.vaan.cannonsRPG.auraSkills.sources
 
-class FiringSource
+import dev.aurelium.auraskills.api.source.CustomSource
+import dev.aurelium.auraskills.api.source.SourceValues
+
+class FiringSource : CustomSource {
+    private val sourceValues: SourceValues
+    private val multiplier: Double
+
+    constructor(sourceValues: SourceValues, multiplier: Double) : super(sourceValues) {
+        this.sourceValues = sourceValues
+        this.multiplier = multiplier
+    }
+
+    fun getMultiplier(): Double {
+        return multiplier
+    }
+}
