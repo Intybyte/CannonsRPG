2a45d07f40bc9b646278afbee232e7a1139a7130
Intybyte <vaan.testwork@gmail.com>
2024-09-02 11:07:30 +0200

    Edit mana ability desc

--- src/main/java/me/vaan/cannonsRPG/auraSkills/CannonManaAbilities.kt
+++ src/main/java/me/vaan/cannonsRPG/auraSkills/CannonManaAbilities.kt
@@ -9,9 +9,11 @@ object CannonManaAbilities {
     val STORM_BLAST = CustomManaAbility
         .builder(NamespacedId.of(Storage.PLUGIN_NAME, "storm_blast"))
         .displayName("Storm Blast")
-        .description("Summons a lightning that deal increased damage in a 5x5 area of cannon hit [Right click gunpowder to activate]")!!
+        .description("Summons a lightning that deal {value} damage in a 5x5 area of cannon hit [Shift right click gunpowder to activate]")!!
         .baseValue(1.0)
         .valuePerLevel(0.5)
+        .baseManaCost(20.0)
+        .manaCostPerLevel(10.0)
         .unlock(6)
         .levelUp(6)
         .maxLevel(0)
